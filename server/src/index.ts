42722e9ddd711cb843a5175759a2f8464bb0bd8f
Samara Motta <samaramotta1998@gmail.com>
2019-03-31 21:27:44 -0300

    Add user basic CRUD

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,76 @@
-import express = require('express');
+require('dotenv').config();
+import express = require("express");
+const mongoose = require("mongoose");
+const BodyParser = require("body-parser");
 
-const app: express.Application = express();
+const {DB_URL, DB_NAME, DB_USER, DB_PASS} = process.env
 
-app.get("/", function(req, res) {
-  res.send("Hello World");
+mongoose.connect(
+  `mongodb+srv://${DB_USER}:${DB_PASS}@${DB_URL}/${DB_NAME}?retryWrites=true`
+);
+
+
+const UserModel = mongoose.model("user", {
+  firstname: { type: String, required: true },
+  lastname: String
 });
 
-app.listen(3000, function() {
-  console.log("Example app listening on port 3000!");
+const app: express.Application = express();
+app.use(BodyParser.json());
+app.use(BodyParser.urlencoded({ extended: true }));
+
+// Vai lá no mongo
+// cata os cara
+// responde o client com os cara
+
+app.get("/api/users", async (request, response) => {
+  try {
+    var result = await UserModel.find().exec();
+    response.send(result);
+  } catch (error) {
+    response.status(500).send(error);
+  }
+});
+
+app.post("/api/users", async (request, response) => {
+  try {
+    var user = new UserModel(request.body);
+    var result = await user.save();
+    response.send(result);
+  } catch (error) {
+    response.status(500).send(error);
+  }
+});
+
+app.get("/api/users/:id", async (request, response) => {
+  try {
+    var person = await UserModel.findById(request.params.id).exec();
+    response.send(person);
+  } catch (error) {
+    response.status(500).send(error);
+  }
+});
+
+app.put("/api/users/:id", async (request, response) => {
+  try {
+    var person = await UserModel.findById(request.params.id).exec();
+    person.set(request.body);
+    var result = await person.save();
+    response.send(result);
+  } catch (error) {
+    response.status(500).send(error);
+  }
+});
+
+app.delete("/api/users/:id", async (request, response) => {
+  try {
+    var result = await UserModel.deleteOne({ _id: request.params.id }).exec();
+    response.send(result);
+  } catch (error) {
+    response.status(500).send(error);
+  }
+});
+
+app.listen(8080, function() {
+  console.log("Example app listening on port 8080!");
 });
