d9e87ff13447677baf48060ab457bc99f5939e21
Samara Motta <samaramotta1998@gmail.com>
2019-05-01 18:45:15 -0300

    Add List Routes CRUD

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,6 +3,7 @@ import express = require("express");
 import mongoose from "mongoose";
 import BodyParser from "body-parser";
 import user from "./routes/user";
+import list from "./routes/list";
 import cookieParser from "cookie-parser";
 
 const { DB_URL, DB_NAME, DB_USER, DB_PASS } = process.env;
@@ -17,6 +18,7 @@ app.use(BodyParser.urlencoded({ extended: true }));
 app.use(BodyParser.json());
 app.use(cookieParser());
 app.use("/api/users", user);
+app.use("/api/lists", list);
 
 app.listen(8080, function() {
   console.log("App listening on port 8080!");
---
+++ server/src/routes/list.ts
@@ -0,0 +1,78 @@
+require("dotenv").config();
+import { Router, Response, Request } from "express";
+import mongoose from "mongoose";
+import todo from "../models/ToDo";
+import withAuth, { ReqWithAuth } from "../auth";
+import List from "../models/List";
+import console = require("console");
+
+const router = Router();
+
+router.post("/", withAuth, (req: ReqWithAuth, res) => {
+  const list = new List({
+    author: req.user,
+    title: req.body.title,
+    description: req.body.description
+  });
+  list
+    .save()
+    .then(result => {
+      res.status(200).json({
+        success: "New list has been created",
+        list
+      });
+    })
+    .catch(error => {
+      res.status(500).json({
+        error: error
+      });
+    });
+});
+
+router.get("/", withAuth, async (req: ReqWithAuth, res) => {
+  try {
+    const lists = await List.find({ author: req.user }).exec();
+    res.send(lists);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+router.get("/:id", withAuth, async (req: ReqWithAuth, res) => {
+  try {
+    const list = await List.find({
+      author: req.user,
+      _id: req.params.id
+    }).exec();
+    res.send(list);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+router.patch("/:id", withAuth, async (req: ReqWithAuth, res) => {
+  try {
+    const list = await List.findOneAndUpdate(
+      { author: req.user, _id: req.params.id },
+      req.body,
+      { new: true }
+    ).exec();
+    res.send(list);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+router.delete("/:id", withAuth, async (req: ReqWithAuth, res) => {
+  try {
+    const list = await List.deleteOne({
+      author: req.user,
+      _id: req.params.id
+    }).exec();
+    res.send(list);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+export default router;
