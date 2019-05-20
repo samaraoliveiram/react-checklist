b48a4b27a00e5dd35ba42d1cf170b3d112b8b6c7
Samara Motta <samaramotta1998@gmail.com>
2019-05-20 09:48:25 -0300

    Add new routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -11,6 +11,9 @@ import Signup from "./pages/Signup";
 import Lists from "./pages/Lists";
 import Todos from "./pages/Todos";
 import NewTodo from "./pages/NewTodo";
+import EditList from "./pages/EditList";
+import CreateListPage from "./pages/CreateList";
+import EditTodo from "./pages/EditTodo";
 
 class App extends Component {
   render() {
@@ -23,7 +26,19 @@ class App extends Component {
                 <Switch>
                   <Route path="/signin" component={Signin} />
                   <Route path="/signup" component={Signup} />
-                  <Route path="/lists/:id/todo" component={withAuth(NewTodo)} />
+                  <Route path="/todo/:id/edit" component={withAuth(EditTodo)} />
+                  <Route
+                    path="/lists/:id/todo/create"
+                    component={withAuth(NewTodo)}
+                  />
+                  <Route
+                    path="/lists/create"
+                    component={withAuth(CreateListPage)}
+                  />
+                  <Route
+                    path="/lists/:id/edit"
+                    component={withAuth(EditList)}
+                  />
                   <Route path="/lists/:id" component={withAuth(Todos)} />
                   <Route path="/lists" component={withAuth(Lists)} />
                   <Route path="/" component={withAuth(Home)} />
